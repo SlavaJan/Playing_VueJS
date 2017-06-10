1e92381b1bb06b26c22d007285533eac65c62cb1
SlavaJan <slavjan81@gmail.com>
2017-06-10 17:32:33 +0300

    wip

---
+++ vuejs-playlist/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <ninjas v-bind:ninjas="ninjas"></ninjas>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/header.vue';
+import Footer from './components/footer.vue';
+import Ninjas from './components/ninjas.vue';
+
+export default {
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'ninjas': Ninjas
+  },
+  data () {
+    return {
+      title: 'Ninja app',
+      ninjas: [
+        {name: 'Ryu', speciality: 'Dragon kick', show: 'false'},
+        {name: 'Ken', speciality: 'Dragon punch', show: 'false'},
+        {name: 'Dan', speciality: 'Weakling slap', show: 'false'},
+        {name: 'Sakura', speciality: 'Spin kick', show: 'false'},
+        {name: 'Akuma', speciality: 'Demon strike', show: 'false'},
+        {name: 'Gouken', speciality: 'God smash', show: 'false'}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
